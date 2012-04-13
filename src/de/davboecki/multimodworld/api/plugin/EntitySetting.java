77022830bf860108dabbaaf7468177c92a9f3a6f
davboecki <dboeken@yahoo.de>
2012-04-14 00:07:30 +0200

    Added equals to EntitySettings

--- src/de/davboecki/multimodworld/api/plugin/EntitySetting.java
+++ src/de/davboecki/multimodworld/api/plugin/EntitySetting.java
@@ -5,4 +5,12 @@ import net.minecraft.server.Entity;
 public class EntitySetting {
 	public Class<Entity> entityclass;
 	public String entityName;
+	
+	public boolean equals(Object object) {
+		if(object instanceof EntitySetting){
+			return ((EntitySetting)object).entityclass.equals(this.entityclass) && ((EntitySetting)object).entityName.equals(this.entityName);
+		} else {
+			return false;
+		}
+	}
 }
